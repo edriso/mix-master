af6e8d9c3e246178faec0d558fd41e23e4dd5dd9
Mohamed Idris <edriso.dev@gmail.com>
2026-02-20 06:51:57 +0200

    Add Navbar component and integrate it into HomeLayout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Navbar';
+
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <div className='nav-center'>
+        <span className='logo'>MixMaster</span>
+
+        <div className='nav-links'>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/newsletter'>Newsletter</NavLink>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default Navbar;
