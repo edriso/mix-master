2fcf71a5036f736f485992543cf159eff14dfea0
Mohamed Idris <edriso.dev@gmail.com>
2026-02-20 06:37:24 +0200

    Add routing and page components for Home, About, Cocktail, Newsletter, and Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,51 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout,
+  Landing,
+  Newsletter,
+  About,
+  Cocktail,
+  Error,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: '/cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: '/newsletter',
+        element: <Newsletter />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+        // you can nest links as deep as needed // and add <Outlet /> inside About.jsx
+        // children: [
+        //   {
+        //     index: true,
+        //     element: <h2>About Company</h2>,
+        //   },
+        //   {
+        //     path: '/Person',
+        //     element: <h2>John Doe</h2>,
+        //   },
+        // ],
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
 export default App;
