332e7235a35f6436070f617f207e2c687aad0a89
Mohamed Idris <edriso.dev@gmail.com>
2026-02-20 08:26:30 +0200

    Implement error handling in Error component with 404 and general error messages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,31 @@
+import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import notFoundImg from '../assets/not-found.svg';
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  // console.log(error); // useful while developing
+
+  if (error.status === 4041) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={notFoundImg} alt='not found' />
+          <h3>Oops!</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to='/'>Back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div>
+        <h3>Something went wrong!</h3>
+        <Link to='/'>Back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
